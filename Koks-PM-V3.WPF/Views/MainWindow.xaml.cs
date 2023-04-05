f4d0040465e55703c0033eeb8c84f87fd34ec28f
Rin Shima <rinshima@yandex.ru>
2023-04-05 19:31:25 +0300

    [Feature] Create base project file structure (MVVM)

--- Koks-PM-V3.WPF/Views/MainWindow.xaml.cs
+++ Koks-PM-V3.WPF/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Koks_PM_V3.WPF
+namespace Koks_PM_V3.WPF.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
