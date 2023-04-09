0f68860fab9e4b17939b42c5023bebc01c396c1a
Rin Shima <rinshima@yandex.ru>
2023-04-09 10:07:27 +0300

    [Feature] Add BankCard model

---
+++ Koks-PM-V3.Domain/Models/BankCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.Domain.Models
+{
+    public class BankCard
+    {
+        public BankCard(Guid cardID, string cardName, 
+            string cardNumber, string cardHolder, 
+            string cardCVC, string cardType, 
+            DateTime cardExpiryDate, Guid categoryID, 
+            Guid userID, DateTime modifyDate, DateTime createDate)
+        {
+            this.cardID = cardID;
+            this.cardName = cardName;
+            this.cardNumber = cardNumber;
+            this.cardHolder = cardHolder;
+            this.cardCVC = cardCVC;
+            this.cardType = cardType;
+            this.cardExpiryDate = cardExpiryDate;
+            this.categoryID = categoryID;
+            this.userID = userID;
+            this.modifyDate = modifyDate;
+            this.createDate = createDate;
+        }
+
+        public Guid cardID { get; set; }
+        public string cardName { get; set; }
+        public string cardNumber { get; set; }
+        public string cardHolder { get; set; }
+        public string cardCVC { get; set; }
+        public string cardType { get; set; }
+        public DateTime cardExpiryDate { get; set; }
+        public Guid categoryID { get; set; }
+        public Guid userID { get; set; }
+        public DateTime modifyDate { get; set; }
+        public DateTime createDate { get; set; }
+    }
+}
