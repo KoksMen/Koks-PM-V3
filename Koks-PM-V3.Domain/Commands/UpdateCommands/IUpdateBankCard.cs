2838629f5f0fb873ba9234b24f1308a6ec39a0ef
Rin Shima <rinshima@yandex.ru>
2023-04-09 10:21:34 +0300

    [Feature] Add iterfaces for domain update commands

---
+++ Koks-PM-V3.Domain/Commands/UpdateCommands/IUpdateBankCard.cs
@@ -0,0 +1,14 @@
+﻿using Koks_PM_V3.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.Domain.Commands.UpdateCommands
+{
+    public interface IUpdateBankCard
+    {
+        Task Execute(BankCard bankCard);
+    }
+}
