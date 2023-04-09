18475d9088250e0fd608cff88e422a7a6bbdc974
Rin Shima <rinshima@yandex.ru>
2023-04-09 10:08:36 +0300

    [Feature] Add Category model

---
+++ Koks-PM-V3.Domain/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.Domain.Models
+{
+    public class Category
+    {
+        public Guid categoryID { get; set; }
+        public string categoryName { get; set; }
+        public Guid userID { get; set; }
+        public DateTime modifyDate { get; set; }
+        public DateTime createDate { get; set; }
+    }
+}
