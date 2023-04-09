1f817660a6736f3f7ee74dd0fdfc4148939fc522
Rin Shima <rinshima@yandex.ru>
2023-04-09 10:30:10 +0300

    [Feature] Add interfaces for domain queries

---
+++ Koks-PM-V3.Domain/Querires/IGetAllCategories.cs
@@ -0,0 +1,14 @@
+﻿using Koks_PM_V3.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.Domain.Querires
+{
+    public interface IGetAllCategories
+    {
+        Task<List<Category>> Execute(Guid userID);
+    }
+}
---
+++ Koks-PM-V3.Domain/Querires/IGetAllNotes.cs
@@ -0,0 +1,14 @@
+﻿using Koks_PM_V3.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.Domain.Querires
+{
+    public interface IGetAllNotes
+    {
+        Task<List<Note>> Execute(Guid userID);
+    }
+}
