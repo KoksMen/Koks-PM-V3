1ce2e350f506c3c2c4a94fe5f4833fe381c026ac
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-10 14:13:30 +0300

    [Feature] Add user Dto

---
+++ Koks-PM-V3.EntityFramework/DTOs/UserDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework.DTOs
+{
+    public class UserDto
+    {
+        [Key]
+        [Required] 
+        public Guid UserDtoID { get; set; }
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string userLogin { get; set; }
+        [Required]
+        public string userPassword { get; set; }
+        public string userTotpKey { get; set; }
+        public string userTelegramBotApi { get; set; }
+        public string userTelegramChatID { get; set; }
+        public byte[] userAvatar { get; set; }
+        [Required]
+        public DateTime modifyDate { get; set; }
+        [Required]
+        public DateTime createDate { get; set; }
+        [Required]
+        public List<NoteDto> userNotes { get; set; }
+        [Required]
+        public List<BankCardDto> userBankCards { get; set; }
+        [Required]
+        public List<CategoryDto> userCategories { get; set; }
+    }
+}
