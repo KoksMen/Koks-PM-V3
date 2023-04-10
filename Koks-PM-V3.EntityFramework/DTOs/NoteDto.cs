64207318a8b9ee1665d48eb113512fec6adabe7a
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-10 14:07:49 +0300

    [Feature] Add note Dto

---
+++ Koks-PM-V3.EntityFramework/DTOs/NoteDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework.DTOs
+{
+    public class NoteDto
+    {
+        [Key]
+        [Required]
+        public Guid NoteDtoID { get; set; }
+        [Required]
+        public string noteName { get; set; }
+        [Required]
+        public string noteLogin { get; set; }
+        [Required]
+        public string notePassword { get; set; }
+        public string noteUrl { get; set; }
+        public string noteTotp { get; set; }
+        [Required]
+        public Guid categoryID { get; set; }
+        [Required]
+        public Guid userID { get; set; }
+        [Required]
+        public DateTime modifyDate { get; set; }
+        [Required]
+        public DateTime createDate { get; set; }
+    }
+}
