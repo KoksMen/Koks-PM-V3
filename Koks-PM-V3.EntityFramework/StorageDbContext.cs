0d2f7302e32aa5b7b96c4838db83d8254fed7049
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-10 14:29:16 +0300

    [Feature] Add storagedbcontext and storagedbcontextfactory

---
+++ Koks-PM-V3.EntityFramework/StorageDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Koks_PM_V3.EntityFramework.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework
+{
+    public class StorageDbContext : DbContext
+    {
+        public StorageDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<UserDto> Users { get; set; }
+        public DbSet<NoteDto> Notes { get; set; }
+        public DbSet<BankCardDto> BankCards { get; set; }
+        public DbSet<CategoryDto> CategoryDtos { get; set; }
+    }
+}
---
+++ Koks-PM-V3.EntityFramework/StorageDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework
+{
+    public class StorageDbContextFactory
+    {
+        private readonly DbContextOptions _options;
+
+        public StorageDbContextFactory(DbContextOptions options)
+        {
+            _options = options;
+        }
+
+        public StorageDbContext Create() => new StorageDbContext(_options);
+    }
+}
