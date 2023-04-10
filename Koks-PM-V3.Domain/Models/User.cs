8816c2cdeeed245c09c82f9145100b02e37a6c63
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-10 14:10:36 +0300

    [Refactor] Make ctor to user model

--- Koks-PM-V3.Domain/Models/User.cs
+++ Koks-PM-V3.Domain/Models/User.cs
@@ -8,6 +8,27 @@ namespace Koks_PM_V3.Domain.Models
 {
     public class User
     {
+        public User(Guid userID, string userName, string userLogin, 
+            string userPassword, string userTotpKey, string userTelegramBotApi, 
+            string userTelegramChatID, byte[] userAvatar, DateTime modifyDate,
+            DateTime createDate, List<Note> userNotes, 
+            List<BankCard> userBankCards, List<Category> userCategories)
+        {
+            this.userID = userID;
+            this.userName = userName;
+            this.userLogin = userLogin;
+            this.userPassword = userPassword;
+            this.userTotpKey = userTotpKey;
+            this.userTelegramBotApi = userTelegramBotApi;
+            this.userTelegramChatID = userTelegramChatID;
+            this.userAvatar = userAvatar;
+            this.modifyDate = modifyDate;
+            this.createDate = createDate;
+            this.userNotes = userNotes;
+            this.userBankCards = userBankCards;
+            this.userCategories = userCategories;
+        }
+
         public Guid userID { get; set; }
         public string userName { get; set; }
         public string userLogin { get; set; }
