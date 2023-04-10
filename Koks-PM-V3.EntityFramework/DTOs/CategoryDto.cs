ce30bb8ffcded1bde751c6ef8c4a18198378d728
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-10 14:11:40 +0300

    [Feature] Add category Dto

---
+++ Koks-PM-V3.EntityFramework/DTOs/CategoryDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework.DTOs
+{
+    public class CategoryDto
+    {
+        [Key]
+        [Required]
+        public Guid CategoryDtoID { get; set; }
+        [Required]
+        public string categoryName { get; set; }
+        [Required]
+        public Guid userID { get; set; }
+        [Required]
+        public DateTime modifyDate { get; set; }
+        [Required]
+        public DateTime createDate { get; set; }
+    }
+}
