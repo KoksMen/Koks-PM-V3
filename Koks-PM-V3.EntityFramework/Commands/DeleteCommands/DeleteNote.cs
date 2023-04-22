2b71124a6640f9cebd1e605ac63eab5e9ea8d366
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-22 08:56:22 +0300

    [Feature] Add remove note bd command

---
+++ Koks-PM-V3.EntityFramework/Commands/DeleteCommands/DeleteNote.cs
@@ -0,0 +1,34 @@
+﻿using Koks_PM_V3.Domain.Commands.DeleteCommands;
+using Koks_PM_V3.EntityFramework.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework.Commands.DeleteCommands
+{
+    public class DeleteNote : IDeleteNote
+    {
+        private readonly StorageDbContextFactory _storageDbContextFactory;
+
+        public DeleteNote(StorageDbContextFactory storageDbContextFactory)
+        {
+            _storageDbContextFactory = storageDbContextFactory;
+        }
+
+        public async Task Execute(Guid noteID)
+        {
+            using (StorageDbContext context = _storageDbContextFactory.Create())
+            {
+                NoteDto noteDto = new NoteDto()
+                {
+                    NoteDtoID = noteID,
+                };
+
+                context.Notes.Remove(noteDto);
+                context.SaveChangesAsync();
+            }
+        }
+    }
+}
