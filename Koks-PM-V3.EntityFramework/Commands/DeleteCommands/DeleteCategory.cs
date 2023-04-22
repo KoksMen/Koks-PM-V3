8d940afb4779b203b1509817acfa0fcfb71d8761
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-04-22 08:55:54 +0300

    [Feature] Add remove category bd command

---
+++ Koks-PM-V3.EntityFramework/Commands/DeleteCommands/DeleteCategory.cs
@@ -0,0 +1,34 @@
+﻿using Koks_PM_V3.Domain.Commands.DeleteCommands;
+using Koks_PM_V3.EntityFramework.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework.Commands.DeleteCommands
+{
+    public class DeleteCategory : IDeleteCategory
+    {
+        private readonly StorageDbContextFactory _storageDbContextFactory;
+
+        public DeleteCategory(StorageDbContextFactory storageDbContextFactory)
+        {
+            _storageDbContextFactory = storageDbContextFactory;
+        }
+
+        public async Task Execute(Guid categoryID)
+        {
+            using (StorageDbContext context =  _storageDbContextFactory.Create())
+            {
+                CategoryDto categoryDto = new CategoryDto()
+                {
+                    CategoryDtoID = categoryID,
+                };
+
+                context.CategoryDtos.Remove(categoryDto);
+                context.SaveChangesAsync();
+            }
+        }
+    }
+}
