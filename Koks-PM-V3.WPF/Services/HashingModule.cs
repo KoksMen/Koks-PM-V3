7d90a56911c6dced8ca9b0b6c636ceacad8def05
Rin Shima <rinshima@yandex.ru>
2023-04-24 09:42:59 +0300

    [Feature] Add hashing module

---
+++ Koks-PM-V3.WPF/Services/HashingModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.Services
+{
+    public static class HashingModule
+    {
+        public static string HashPassword(string password)
+        {
+            string HashedPassword;
+            SHA512 hash = SHA512.Create();
+            var PasswordBytes = Encoding.Default.GetBytes(password);
+            HashedPassword = Convert.ToHexString(hash.ComputeHash(PasswordBytes));
+            return HashedPassword;
+        }
+    }
+}
