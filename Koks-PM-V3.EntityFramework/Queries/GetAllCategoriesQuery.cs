50315f18235126385c32e9caa0eff778904e066b
Rin Shima <rinshima@yandex.ru>
2023-04-25 09:05:52 +0300

    [Feature] Add get all categories db query

---
+++ Koks-PM-V3.EntityFramework/Queries/GetAllCategoriesQuery.cs
@@ -0,0 +1,40 @@
+﻿using Koks_PM_V3.Domain.Models;
+using Koks_PM_V3.Domain.Querires;
+using Koks_PM_V3.EntityFramework.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework.Queries
+{
+    public class GetAllCategoriesQuery : IGetAllCategories
+    {
+        private readonly StorageDbContextFactory _storageContextFactory;
+
+        public GetAllCategoriesQuery(StorageDbContextFactory storageContextFactory)
+        {
+            _storageContextFactory = storageContextFactory;
+        }
+
+        public async Task<List<Category>> Execute(Guid userID)
+        {
+            using (StorageDbContext context = _storageContextFactory.Create())
+            {
+                List<CategoryDto> categoryDtos = await context.CategoryDtos.ToListAsync();
+
+                List<Category> categories = categoryDtos.Select(x => new Category(
+                    x.CategoryDtoID,
+                    x.categoryName,
+                    x.userID,
+                    x.modifyDate,
+                    x.createDate
+                    )).ToList();
+
+                return categories;
+            }
+        }
+    }
+}
