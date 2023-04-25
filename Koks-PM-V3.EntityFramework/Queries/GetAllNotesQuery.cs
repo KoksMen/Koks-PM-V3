2f80cde4faaff51106998b5f3f0174ba0d835754
Rin Shima <rinshima@yandex.ru>
2023-04-25 09:00:32 +0300

    [Feature] Add get all notes db query

---
+++ Koks-PM-V3.EntityFramework/Queries/GetAllNotesQuery.cs
@@ -0,0 +1,45 @@
+﻿using Koks_PM_V3.Domain.Models;
+using Koks_PM_V3.Domain.Querires;
+using Koks_PM_V3.EntityFramework.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.EntityFramework.Queries
+{
+    public class GetAllNotesQuery : IGetAllNotes
+    {
+        private readonly StorageDbContextFactory _storageContextFactory;
+
+        public GetAllNotesQuery(StorageDbContextFactory storageContextFactory)
+        {
+            _storageContextFactory = storageContextFactory;
+        }
+
+        public async Task<List<Note>> Execute(Guid userID)
+        {
+            using (StorageDbContext context = _storageContextFactory.Create())
+            {
+                List<NoteDto> notesDtos = await context.Notes.ToListAsync();
+
+                List<Note> notes = notesDtos.Select(x => new Note(
+                    x.NoteDtoID,
+                    x.noteName,
+                    x.noteLogin,
+                    x.notePassword,
+                    x.noteUrl,
+                    x.noteTotp,
+                    x.categoryID,
+                    x.userID,
+                    x.modifyDate,
+                    x.createDate
+                    )).ToList();
+
+                return notes;
+            }
+        }
+    }
+}
