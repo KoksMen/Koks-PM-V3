037d5aad6da56d858ddf279545c46977eb43b708
Rin Shima <rinshima@yandex.ru>
2023-04-30 17:02:35 +0300

    [Feature] Add base datastore and datastorefactory

---
+++ Koks-PM-V3.WPF/Stores/DataStores/DataStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Koks_PM_V3.Domain.Commands.CreateCommands;
+using Koks_PM_V3.Domain.Commands.DeleteCommands;
+using Koks_PM_V3.Domain.Commands.UpdateCommands;
+using Koks_PM_V3.Domain.Models;
+using Koks_PM_V3.Domain.Querires;
+using Koks_PM_V3.EntityFramework.DTOs;
+
+namespace Koks_PM_V3.WPF.Stores.DataStores
+{
+    public class DataStore
+    {
+        #region Commands/Queries Interfaces
+        private readonly ICreateNote _createNoteCommand;
+        private readonly IDeleteNote _deleteNoteCommand;
+        private readonly IUpdateNote _updateNoteCommand;
+        private readonly ICreateBankCard _createBankCardCommand;
+        private readonly IDeleteBankCard _deleteBankCardCommand;
+        private readonly IUpdateBankCard _updateBankCardCommand;
+        private readonly ICreateCategory _createCategoryCommand;
+        private readonly IDeleteCategory _deleteCategoryCommand;
+        private readonly IUpdateCategory _updateCategoryCommand;
+        private readonly ICreateUser _createUserCommand;
+        private readonly IDeleteUser _deleteUserCommand;
+        private readonly IUpdateUser _updateUserCommand;
+        private readonly IGetAllNotes _getAllNotesQuerry;
+        private readonly IGetAllBankCards _getAllBankCardsQuerry;
+        private readonly IGetAllCategories _getAllCategoriesQuerry;
+        #endregion
+
+
+    }
+}
---
+++ Koks-PM-V3.WPF/Stores/DataStores/DataStoreFactory.cs
@@ -0,0 +1,63 @@
+﻿using Koks_PM_V3.Domain.Commands.CreateCommands;
+using Koks_PM_V3.Domain.Commands.DeleteCommands;
+using Koks_PM_V3.Domain.Commands.UpdateCommands;
+using Koks_PM_V3.Domain.Querires;
+using Koks_PM_V3.EntityFramework.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.Stores.DataStores
+{
+    public class DataStoreFactory
+    {
+        private readonly ICreateNote _createNoteCommand;
+        private readonly IDeleteNote _deleteNoteCommand;
+        private readonly IUpdateNote _updateNoteCommand;
+        private readonly ICreateBankCard _createBankCardCommand;
+        private readonly IDeleteBankCard _deleteBankCardCommand;
+        private readonly IUpdateBankCard _updateBankCardCommand;
+        private readonly ICreateCategory _createCategoryCommand;
+        private readonly IDeleteCategory _deleteCategoryCommand;
+        private readonly IUpdateCategory _updateCategoryCommand;
+        private readonly ICreateUser _createUserCommand;
+        private readonly IDeleteUser _deleteUserCommand;
+        private readonly IUpdateUser _updateUserCommand;
+        private readonly IGetAllNotes _getAllNotesQuerry;
+        private readonly IGetAllBankCards _getAllBankCardsQuerry;
+        private readonly IGetAllCategories _getAllCategoriesQuerry;
+
+        public DataStoreFactory(ICreateNote createNoteCommand, IDeleteNote deleteNoteCommand, 
+            IUpdateNote updateNoteCommand, ICreateBankCard createBankCardCommand, 
+            IDeleteBankCard deleteBankCardCommand, IUpdateBankCard updateBankCardCommand, 
+            ICreateCategory createCategoryCommand, IDeleteCategory deleteCategoryCommand, 
+            IUpdateCategory updateCategoryCommand, ICreateUser createUserCommand, 
+            IDeleteUser deleteUserCommand, IUpdateUser updateUserCommand, 
+            IGetAllNotes getAllNotesQuerry, IGetAllBankCards getAllBankCardsQuerry, 
+            IGetAllCategories getAllCategoriesQuerry)
+        {
+            _createNoteCommand = createNoteCommand;
+            _deleteNoteCommand = deleteNoteCommand;
+            _updateNoteCommand = updateNoteCommand;
+            _createBankCardCommand = createBankCardCommand;
+            _deleteBankCardCommand = deleteBankCardCommand;
+            _updateBankCardCommand = updateBankCardCommand;
+            _createCategoryCommand = createCategoryCommand;
+            _deleteCategoryCommand = deleteCategoryCommand;
+            _updateCategoryCommand = updateCategoryCommand;
+            _createUserCommand = createUserCommand;
+            _deleteUserCommand = deleteUserCommand;
+            _updateUserCommand = updateUserCommand;
+            _getAllNotesQuerry = getAllNotesQuerry;
+            _getAllBankCardsQuerry = getAllBankCardsQuerry;
+            _getAllCategoriesQuerry = getAllCategoriesQuerry;
+        }
+
+        public DataStore Create(UserDto userDto)
+        {
+            throw new NotImplementedException("DataStoreFactory.Create - is not realized");
+        }
+    }
+}
