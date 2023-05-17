e4019e15716fb5c3ed7cc91cfebbca790400076c
Rin Shima <rinshima@yandex.ru>
2023-05-17 08:04:06 +0300

    [Feature] Add mainpage navigator

---
+++ Koks-PM-V3.WPF/Stores/Navigators/MainPageNavigator.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.Stores.Navigators
+{
+    public class MainPageNavigator: BindableBase
+    {
+        private BindableBase _selectedMainPage;
+        public BindableBase SelectedMainPage
+        {
+            get { return _selectedMainPage; }
+            set { _selectedMainPage = value; MainPageChanged?.Invoke(); }
+        }
+
+        public event Action MainPageChanged;
+    }
+}
