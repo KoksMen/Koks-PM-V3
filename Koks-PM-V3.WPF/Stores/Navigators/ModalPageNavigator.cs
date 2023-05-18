956f800bf6535b5d3b25ca886cc3f0b5d5c96ed8
Rin Shima <rinshima@yandex.ru>
2023-05-18 06:49:37 +0300

    [Feature] Add modal page navigator

---
+++ Koks-PM-V3.WPF/Stores/Navigators/ModalPageNavigator.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.Stores.Navigators
+{
+    public class ModalPageNavigator : BindableBase
+    {
+        private BindableBase _selectedModalPage;
+        public BindableBase SelectedModalPage
+        {
+            get { return _selectedModalPage; }
+            set { _selectedModalPage = value; ModalPageChanged?.Invoke();  }
+        }
+
+        public event Action ModalPageChanged;
+    }
+}
