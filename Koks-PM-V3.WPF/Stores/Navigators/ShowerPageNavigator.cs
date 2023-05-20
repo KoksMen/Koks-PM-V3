9f76429eb28f57e0a20c3403696d857868aba57d
Rin Shima <rinshima@yandex.ru>
2023-05-20 06:58:18 +0300

    [Feature] Add shower page navigator

---
+++ Koks-PM-V3.WPF/Stores/Navigators/ShowerPageNavigator.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.Stores.Navigators
+{
+    public class ShowerPageNavigator : BindableBase
+    {
+		private BindableBase _selectedShowerPage;
+
+		public BindableBase selectedShowerPage
+		{
+			get { return _selectedShowerPage; }
+			set { _selectedShowerPage = value; ShowerPageChanged?.Invoke(); }
+		}
+
+		public event Action ShowerPageChanged;
+
+	}
+}
