d906b2ea5e1c4d1250650cb4801eabc69ede2112
Rin Shima <rinshima@yandex.ru>
2023-05-23 07:05:23 +0300

    [Feature] Add login page data store

---
+++ Koks-PM-V3.WPF/Stores/DataStores/LoginDataStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.Stores.DataStores
+{
+    public class LoginDataStore
+    {
+		private string _login;
+
+		public string Login
+		{
+			get { return _login; }
+			set { _login = value; }
+		}
+
+		private string _password;
+
+		public string Password
+		{
+			get { return _password; }
+			set { _password = value; }
+		}
+
+		//Maybe change
+		private DataStoreFactory _dataStoreFactory;
+
+		public DataStoreFactory DataStoreFactory
+		{
+			get { return _dataStoreFactory; }
+			set 
+			{ 
+				if (_dataStoreFactory == null) 
+					_dataStoreFactory = value; 
+			}
+		}
+
+	}
+}
