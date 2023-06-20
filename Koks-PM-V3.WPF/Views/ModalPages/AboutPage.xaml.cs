737f41272a721b732940d52d75b6dba048cd86cb
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-06-20 08:32:45 +0300

    [UI] Add about page

---
+++ Koks-PM-V3.WPF/Views/ModalPages/AboutPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koks_PM_V3.WPF.Views.ModalPages
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutModalPage.xaml
+    /// </summary>
+    public partial class AboutModalPage : UserControl
+    {
+        public AboutModalPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
