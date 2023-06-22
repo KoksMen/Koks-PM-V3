4db3d8df6da5962055b0d27b9a31bf7eb9681f0c
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-06-22 10:43:41 +0300

    [Feature] Add base aboutpage viewmodel

---
+++ Koks-PM-V3.WPF/ViewModels/ModalViewModels/AboutPageVM.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Mvvm;
+using Koks_PM_V3.WPF.Stores.Navigators;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Koks_PM_V3.WPF.ViewModels.ModalViewModels
+{
+    public class AboutPageVM : BindableBase
+    {
+        private readonly ModalPageNavigator _modalPageNavigator;
+
+        public AboutPageVM(ModalPageNavigator modalPageNavigator)
+        {
+            _modalPageNavigator = modalPageNavigator;
+        }
+
+        public ICommand CloseCommand => throw new NotImplementedException("AboutPageVM - CloseCommand - NotImplementException");
+    }
+}
