44711641f030544cb3860d7f78b8d19a7b344662
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-06-22 10:45:32 +0300

    [Feature] Add ViewModel files and change project structure

---
+++ Koks-PM-V3.WPF/ViewModels/ModalViewModels/AddTelegramVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.ViewModels.ModalViewModels
+{
+    public class AddTelegramVM
+    {
+    }
+}
