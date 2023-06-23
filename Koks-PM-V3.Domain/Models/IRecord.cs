e60df0abc31fa9b5261a35c32060f577310bb9c4
Rin Shima <rinshima@yandex.ru>
2023-06-23 10:12:48 +0300

    [Feature] Add IRecord model

---
+++ Koks-PM-V3.Domain/Models/IRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.Domain.Models
+{
+    public interface IRecord
+    {
+        public Guid ID { get; }
+        public string Title { get; }
+        public string Desctiption { get; }
+    }
+}
