aea1ea11d298a7d7378d21448ff11c1437a65c60
Rin Shima <rinshima@yandex.ru>
2023-06-23 07:48:57 +0300

    [Feature] Add base AddTelegramVM

--- Koks-PM-V3.WPF/ViewModels/ModalViewModels/AddTelegramVM.cs
+++ Koks-PM-V3.WPF/ViewModels/ModalViewModels/AddTelegramVM.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using DevExpress.Mvvm;
+using Koks_PM_V3.WPF.Stores.DataStores;
+using Koks_PM_V3.WPF.Stores.Navigators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Koks_PM_V3.WPF.ViewModels.ModalViewModels
 {
-    public class AddTelegramVM
+    public class AddTelegramVM: BindableBase
     {
+        private readonly DataStore _dataStore;
+        private readonly ModalPageNavigator _modalPageNavigator;
+
+        public AddTelegramVM(DataStore dataStore, ModalPageNavigator modalPageNavigator)
+        {
+            _dataStore = dataStore;
+            _modalPageNavigator = modalPageNavigator;
+        }
+
+        private string _telegramBotAPI = string.Empty;
+
+        public string TelegramBotAPI
+        {
+            get { return _telegramBotAPI; }
+            set { _telegramBotAPI = value; 
+                RaisePropertiesChanged(nameof(TelegramBotAPI)); 
+                RaisePropertiesChanged(nameof(SaveCommand)); }
+        }
+
+        private string _telegramChatID = string.Empty;
+
+        public string TelegramChatID
+        {
+            get { return _telegramChatID; }
+            set { _telegramChatID = value; 
+                RaisePropertiesChanged(nameof(TelegramChatID)); 
+                RaisePropertiesChanged(nameof(SaveCommand)); }
+        }
+
+        public ICommand SaveCommand => throw new NotImplementedException("AddTelegramVM => SaveCommand => NotImplementedException");
+        public ICommand CancelCommand => throw new NotImplementedException("AddTelegramVM => CancelCommand => NotImplementedException");
+
     }
 }
