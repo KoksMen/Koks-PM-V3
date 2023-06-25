9ea92b367157981b772096f3ab0088f00661b417
Rin Shima <rinshima@yandex.ru>
2023-06-25 16:04:17 +0300

    [Refactor] Add category and user ID

--- Koks-PM-V3.Domain/Models/IRecord.cs
+++ Koks-PM-V3.Domain/Models/IRecord.cs
@@ -9,6 +9,8 @@ namespace Koks_PM_V3.Domain.Models
     public interface IRecord
     {
         public Guid ID { get; }
+        public Guid CategoryID { get; }
+        public Guid UserID { get; }
         public string Title { get; }
         public string Desctiption { get; }
     }
