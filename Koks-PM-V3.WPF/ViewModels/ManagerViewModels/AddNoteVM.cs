e4b4a0170a45d715cd616c54f148fa0039d1e371
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-06-26 10:01:14 +0300

    [Feature] Add "AddNote" viewmodel

---
+++ Koks-PM-V3.WPF/ViewModels/ManagerViewModels/AddNoteVM.cs
@@ -0,0 +1,77 @@
+﻿using DevExpress.Mvvm;
+using Koks_PM_V3.Domain.Models;
+using Koks_PM_V3.WPF.Stores.DataStores;
+using Koks_PM_V3.WPF.Stores.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Koks_PM_V3.WPF.ViewModels.ManagerViewModels
+{
+    public class AddNoteVM : BindableBase
+    {
+        private readonly ShowerPageNavigator _viewerNavigator;
+        private readonly DataStore _dataStore;
+        private readonly List<Category> _categories;
+
+        public AddNoteVM(ShowerPageNavigator viewerNavigator, DataStore dataStore, List<Category> senderCategories)
+        {
+            _viewerNavigator = viewerNavigator;
+            _dataStore = dataStore;
+            _categories = senderCategories;
+        }
+
+        private string _Name = string.Empty;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; RaisePropertiesChanged(nameof(Name)); RaisePropertiesChanged(nameof(SaveAddCommand)); }
+        }
+        private string _Login = string.Empty;
+
+        public string Login
+        {
+            get { return _Login; }
+            set { _Login = value; RaisePropertiesChanged(nameof(Login)); RaisePropertiesChanged(nameof(SaveAddCommand)); }
+        }
+        private string _Password = string.Empty;
+
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; RaisePropertiesChanged(nameof(Password)); RaisePropertiesChanged(nameof(SaveAddCommand)); }
+        }
+        private Category _Category = new Category(Guid.Empty, "Null", Guid.Empty, DateTime.Now, DateTime.Now);
+
+        public Category SelectedCategory
+        {
+            get { return _Category; }
+            set { _Category = value; RaisePropertiesChanged(nameof(SelectedCategory)); RaisePropertiesChanged(nameof(SaveAddCommand)); }
+        }
+
+        private string _URL = string.Empty;
+
+        public string URL
+        {
+            get { return _URL; }
+            set { _URL = value; RaisePropertiesChanged(nameof(URL)); RaisePropertiesChanged(nameof(SaveAddCommand)); }
+        }
+
+        private string _Totp = string.Empty;
+
+        public string Totp
+        {
+            get { return _Totp; }
+            set { _Totp = value; RaisePropertiesChanged(nameof(Totp)); RaisePropertiesChanged(nameof(SaveAddCommand)); }
+        }
+
+        public ICollection<Category> Categories => _categories;
+
+        public ICommand SaveAddCommand => throw new NotImplementedException("AddNoteVM - SaveAddCommand - NotImplementException");
+        public ICommand CancelCommand => throw new NotImplementedException("AddNoteVM - CancelCommand - NotImplementException");
+    }
+}
