eea023a4c96a6f608b4bd71808f37344f0d467e0
Rin Shima <rinshima@yandex.ru>
2023-06-27 07:53:03 +0300

    [Feature] Add "Login" view model

---
+++ Koks-PM-V3.WPF/ViewModels/MainViewModels/LoginVM.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.Mvvm;
+using Koks_PM_V3.WPF.Stores.DataStores;
+using Koks_PM_V3.WPF.Stores.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Koks_PM_V3.WPF.ViewModels.MainViewModels
+{
+    public class LoginVM : BindableBase
+    {
+        private readonly MainPageNavigator _pageNavigator;
+        private readonly LoginDataStore _loginPageDataStore;
+        private readonly DataStoreFactory _dataStoreFactory;
+
+
+        public LoginVM(MainPageNavigator pageNavigator, DataStoreFactory dataStoreFactory)
+        {
+            _pageNavigator = pageNavigator;
+            _loginPageDataStore = new LoginDataStore();
+            _dataStoreFactory = dataStoreFactory;
+            _loginPageDataStore.DataStoreFactory = _dataStoreFactory;
+        }
+
+        private string _Login;
+
+        public string Login
+        {
+            get { return _Login; }
+            set { _Login = value; _loginPageDataStore.Login = value; RaisePropertiesChanged(nameof(LoginCommand)); }
+        }
+
+        private string _Password;
+
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; _loginPageDataStore.Password = value; RaisePropertiesChanged(nameof(LoginCommand)); }
+        }
+
+        public ICommand LoginCommand => throw new NotImplementedException("LoginVM => LoginCommand => NotImplementedException");
+
+        public ICommand OpenRegisterCommand => throw new NotImplementedException("LoginVM => OpenRegisterCommand => NotImplementedException");
+
+    }
+}
