a84635a36e81daa2aca4cc1fe9ebe60050ba8199
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-06-27 11:31:51 +0300

    [Feature] Add database options builder

---
+++ Koks-PM-V3.WPF/Services/DbOptionsBuilder.cs
@@ -0,0 +1,35 @@
+﻿using Koks_PM_V3.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koks_PM_V3.WPF.Services
+{
+    public static class DbOptionsBuilder
+    {
+        public static DbContextOptions getDbOptions()
+        {
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<StorageDbContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return options;
+        }
+    }
+}
