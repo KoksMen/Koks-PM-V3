b761d4c882dc081e443437e33b6e20ad047da880
KoksMen Laptop <xxrinshimaxx@gmail.com>
2023-06-27 11:33:01 +0300

    [UI] Add "MainWindow"

--- Koks-PM-V3.WPF/Views/MainWindow.xaml.cs
+++ Koks-PM-V3.WPF/Views/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace Koks_PM_V3.WPF.Views
         {
             InitializeComponent();
         }
+
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
