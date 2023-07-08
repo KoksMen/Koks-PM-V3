cccd4e5598415a28db3bb05d35375c97ab23ecd4
Rin Shima <rinshima@yandex.ru>
2023-07-08 09:57:02 +0300

    [Feature] Add commands to property

--- Koks-PM-V3.WPF/ViewModels/ModalViewModels/AddCategoryVM.cs
+++ Koks-PM-V3.WPF/ViewModels/ModalViewModels/AddCategoryVM.cs
@@ -1,4 +1,6 @@
 ﻿using DevExpress.Mvvm;
+using Koks_PM_V3.WPF.Commands.ClosePageCommands;
+using Koks_PM_V3.WPF.Commands.ManagerCommands.CategoryCommands;
 using Koks_PM_V3.WPF.Stores.DataStores;
 using Koks_PM_V3.WPF.Stores.Navigators;
 using System;
@@ -29,7 +31,7 @@ namespace Koks_PM_V3.WPF.ViewModels.ModalViewModels
             set { _name = value; RaisePropertiesChanged(nameof(Name)); RaisePropertiesChanged(nameof(SaveCommand)); }
         }
 
-        internal ICommand SaveCommand => throw new NotImplementedException("AddCategoryVM - SaveCommand - NotImplementException");
-        internal ICommand CancelCommand => throw new NotImplementedException("AddCategoryVM - CloseCommand - NotImplementException");
+        public ICommand SaveCommand => new SaveAddCategoryCommand(_modalPageNavigator, _dataStore, _name);
+        public ICommand CancelCommand => new CloseModalPageCommand(_modalPageNavigator);
     }
 }
