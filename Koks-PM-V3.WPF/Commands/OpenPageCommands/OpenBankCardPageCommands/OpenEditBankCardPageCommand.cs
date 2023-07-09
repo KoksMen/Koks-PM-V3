9bd670936c19a4294e96255c2f218487689b257b
Rin Shima <rinshima@yandex.ru>
2023-07-09 09:57:00 +0300

    [Feature] Add "open edit bankcard" command

---
+++ Koks-PM-V3.WPF/Commands/OpenPageCommands/OpenBankCardPageCommands/OpenEditBankCardPageCommand.cs
@@ -0,0 +1,48 @@
+﻿using Koks_PM_V3.Domain.Models;
+using Koks_PM_V3.WPF.Stores.DataStores;
+using Koks_PM_V3.WPF.Stores.Navigators;
+using Koks_PM_V3.WPF.ViewModels.ManagerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Koks_PM_V3.WPF.Commands.OpenPageCommands.OpenBankCardPageCommands
+{
+    public class OpenEditBankCardPageCommand : ICommand
+    {
+        private readonly ShowerPageNavigator _showerPageNavigator;
+        private readonly DataStore _dataStore;
+        private readonly List<Category> _categoryModels;
+        private readonly BankCard _bankCardModel;
+
+        public OpenEditBankCardPageCommand(ShowerPageNavigator showerPageNavigator, DataStore dataStore, List<Category> categoryModels, BankCard bankCardModel)
+        {
+            _showerPageNavigator = showerPageNavigator;
+            _dataStore = dataStore;
+            _categoryModels = categoryModels;
+            _bankCardModel = bankCardModel;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            try
+            {
+                _showerPageNavigator.selectedShowerPage = new EditBankCardVM(_showerPageNavigator, _dataStore, _categoryModels, _bankCardModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
