96978bd599cc8e651a2f6cbfa8d51e97e0e64414
Rin Shima <rinshima@yandex.ru>
2023-07-11 10:56:38 +0300

    [UI] Add "Edit user" page

---
+++ Koks-PM-V3.WPF/Views/ModalPages/EditUserPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koks_PM_V3.WPF.Views.ModalPages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditUserPage.xaml
+    /// </summary>
+    public partial class EditUserPage : UserControl
+    {
+        public EditUserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AccountPassword_Changed(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                if (((PasswordBox)sender).Name == "AMPass")
+                {
+                    ((dynamic)this.DataContext).AccountPassword = ((PasswordBox)sender).Password;
+                }
+                else if (((PasswordBox)sender).Name == "ACPass")
+                {
+                    ((dynamic)this.DataContext).AccountSecondPassword = ((PasswordBox)sender).Password;
+                }
+            }
+        }
+    }
+}
