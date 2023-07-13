36d45e4002ac0109df919aeea4a608fe6702e173
Rin Shima <rinshima@yandex.ru>
2023-07-13 19:00:39 +0300

    [Feature] Add "Delete telegram" command

---
+++ Koks-PM-V3.WPF/Commands/ManagerCommands/AccountCommands/DeleteTelegramCommand.cs
@@ -0,0 +1,58 @@
+﻿using Koks_PM_V3.Domain.Models;
+using Koks_PM_V3.WPF.Services;
+using Koks_PM_V3.WPF.Stores.DataStores;
+using Koks_PM_V3.WPF.Stores.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Koks_PM_V3.WPF.Commands.ManagerCommands.AccountCommands
+{
+    public class DeleteTelegramCommand : ICommand
+    {
+        private readonly DataStore dataStore;
+        private ModalPageNavigator modalPageNavigator;
+        private User userModel => dataStore.UserAccount;
+
+        public DeleteTelegramCommand(DataStore dataStore, ModalPageNavigator modalPageNavigator)
+        {
+            this.dataStore = dataStore;
+            this.modalPageNavigator = modalPageNavigator;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            User userToUpdate = new User
+            (
+                userID: userModel.userID,
+                userName: userModel.userName,
+                userLogin: userModel.userLogin,
+                userPassword: userModel.userPassword,
+                userTotpKey: userModel.userTotpKey,
+                userTelegramBotApi: null,
+                userTelegramChatID: null,
+                userAvatar: userModel.userAvatar,
+                modifyDate: DateTime.Now,
+                createDate: userModel.createDate,
+                userNotes: userModel.userNotes,
+                userBankCards: userModel.userBankCards,
+                userCategories: userModel.userCategories
+            );
+
+            dataStore?.UpdateTelegram(userToUpdate);
+
+            modalPageNavigator.SelectedModalPage = null;
+        }
+    }
+}
