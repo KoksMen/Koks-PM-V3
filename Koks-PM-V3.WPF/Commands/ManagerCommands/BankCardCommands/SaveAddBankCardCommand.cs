0386cbc2c474b3b91ba128149fd3ed9382984dfa
Rin Shima <rinshima@yandex.ru>
2023-07-15 12:47:34 +0300

    [Feature] Add data validation to add card data

--- Koks-PM-V3.WPF/Commands/ManagerCommands/BankCardCommands/SaveAddBankCardCommand.cs
+++ Koks-PM-V3.WPF/Commands/ManagerCommands/BankCardCommands/SaveAddBankCardCommand.cs
@@ -1,8 +1,10 @@
 ﻿using Koks_PM_V3.Domain.Models;
 using Koks_PM_V3.WPF.Stores.DataStores;
 using Koks_PM_V3.WPF.Stores.Navigators;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +23,12 @@ namespace Koks_PM_V3.WPF.Commands.ManagerCommands.BankCardCommands
         private readonly string _cvvBankCard;
         private readonly string _holderBankCard;
         private readonly string _typeBankCard;
-        private readonly DateTime _expiryDateBankCard;
+        private readonly string _expiryDateBankCard;
+        private DateTime parsedDate;
 
         public SaveAddBankCardCommand(ShowerPageNavigator showerNavigator, DataStore dataStore, Guid categoryID, 
             string nameBankCard, string numberBankCard, string cvvBankCard, 
-            string holderBankCard, string typeBankCard, DateTime expiryDateBankCard)
+            string holderBankCard, string typeBankCard, string expiryDateBankCard)
         {
             _showerNavigator = showerNavigator;
             _dataStore = dataStore;
@@ -42,33 +45,106 @@ namespace Koks_PM_V3.WPF.Commands.ManagerCommands.BankCardCommands
 
         public bool CanExecute(object? parameter)
         {
+
+            if (_categoryID == new Guid("11111111-1111-1111-1111-111111111111") || _categoryID == Guid.Empty) {
+                return false;
+            }
+            if (_nameBankCard.IsNullOrEmpty() || _nameBankCard.Length < 5) {
+                return false;
+            }
+            if (IsInvalidCreditCardNumber(_numberBankCard)) {
+                return false;
+            }
+            if (_holderBankCard.IsNullOrEmpty() || _holderBankCard.Length < 5) {
+                return false;
+            }
+            if (_cvvBankCard.IsNullOrEmpty() || _cvvBankCard.Length != 3) {
+                return false;
+            }
+            if (IsInvalidDateFormat(_expiryDateBankCard.ToString())) {
+                return false;
+            }
+            if (IsInvalidCardType(_typeBankCard)) {
+                return false;
+            }
+            
             return true;
         }
+
+        private bool IsInvalidCreditCardNumber(string cardNumber)
+        {
+            cardNumber = cardNumber.Replace(" ", "");
+
+            if (!cardNumber.All(char.IsDigit)) {
+                return true;
+            }
+
+            if (cardNumber.Length < 13 || cardNumber.Length > 19) {
+                return true;
+            }
+
+            return false;
+            //Эта проверка вероятно излишня
+            // Проверка, используя алгоритм Луна
+            /*int sum = 0;
+            bool isAlternateDigit = false;
+
+            for (int i = cardNumber.Length - 1; i >= 0; i--) 
+            {
+                int digit = int.Parse(cardNumber[i].ToString());
+
+                if (isAlternateDigit) {
+                    digit *= 2;
+                    if (digit > 9) {
+                        digit = digit % 10 + 1;
+                    }
+                }
+
+                sum += digit;
+                isAlternateDigit = !isAlternateDigit;
+            }
+
+            return sum % 10 != 0;*/
+        }
+        private bool IsInvalidDateFormat(string date)
+        {
+            return !DateTime.TryParseExact(date, "MM/yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate);
+        }
+        private bool IsInvalidCardType(string type) {
+            string[] cardTypes = { "Visa", "MasterCard", "MIR", "UnionPay", "Maestro", "Belkart", "American Express", "Other" };
+
+            foreach (string cardType in cardTypes)
+            {
+                if (type.Equals(cardType, StringComparison.OrdinalIgnoreCase))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
 
         public void Execute(object? parameter)
         {
             try
             {
-                MessageBox.Show("SaveBankCardCommand => CanExecure => no check param/function");
-
                 BankCard addBankCard = new BankCard(
-                    Guid.NewGuid(),
-                    _nameBankCard,
-                    _numberBankCard,
-                    _holderBankCard,
-                    _cvvBankCard,
-                    _typeBankCard,
-                    _expiryDateBankCard,
-                    _categoryID,
-                    _dataStore.UserAccount.userID,
-                    DateTime.Now,
-                    DateTime.Now);
+                    cardID: Guid.NewGuid(),
+                    cardName: _nameBankCard,
+                    cardNumber: _numberBankCard,
+                    cardHolder: _holderBankCard,
+                    cardCVC: _cvvBankCard,
+                    cardType: _typeBankCard,
+                    cardExpiryDate: parsedDate,
+                    categoryID: _categoryID,
+                    userID: _dataStore.UserAccount.userID,
+                    modifyDate: DateTime.Now,
+                    createDate: DateTime.Now);
 
                 _dataStore?.AddBankCard(addBankCard);
             }
-            catch (Exception)
-            {
-
+            catch (Exception) {
                 throw;
             }
         }
